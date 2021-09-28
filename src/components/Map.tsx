6906d006334dbf44fb20178792f32cab3d83368e
eddie.eldridge <steadyeddie101@hotmail.co.uk>
2021-09-28 19:16:54 +0100

    Working LOTR map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,23 +1,24 @@
-import * as React from 'react';
+import { ImageOverlay, MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { CRS, LatLngBounds } from 'leaflet';
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
 
-import { MAPBOX_API_TOKEN } from '../lib/constants';
+const bounds = new LatLngBounds([500.712216, -500.22655], [500.773941, -500.12544])
+
 
 export const LOTRMap = (props: any) => {
-    const [viewport, setViewport] = useState({
-        width: 400,
-        height: 400,
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 8
-    });
+    const [map, setMap] = useState(undefined)
+
+    const setMapReference = (map) => {
+        setMap(map);
+        map.fitBounds(bounds);
+    }
 
     return (
-        <ReactMapGL
-            {...viewport}
-            onViewportChange={nextViewport => setViewport(nextViewport)}
-            mapboxApiAccessToken={MAPBOX_API_TOKEN}
-        />
+        <MapContainer whenCreated={setMapReference} crs={CRS.Simple}>
+            <ImageOverlay
+                url="https://i.pinimg.com/originals/bc/59/91/bc5991a551a011777e72aefa7e914ba8.jpg"
+                bounds={bounds}
+            />
+        </MapContainer>
     );
 };

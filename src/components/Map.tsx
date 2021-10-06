157681af573b0a5a5f337c771dcabebdb4a03afd
eddie.eldridge <steadyeddie101@hotmail.co.uk>
2021-10-06 19:58:23 +0100

    Fixed map styling, added embed

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,24 +1,46 @@
 import { ImageOverlay, MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { CRS, LatLngBounds } from 'leaflet';
+import { CRS, LatLng, latLng, LatLngBounds } from 'leaflet';
 import { useState } from 'react';
+import L from "leaflet";
+import './../assets/scss/App.scss';
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
-const bounds = new LatLngBounds([500.712216, -500.22655], [500.773941, -500.12544])
-
+const mapBounds = new LatLngBounds([0, 0], [1500, 1800]);
 
 export const LOTRMap = (props: any) => {
     const [map, setMap] = useState(undefined)
+    const [myMarkers, setMyMarkers] = useState(L.layerGroup());
 
     const setMapReference = (map) => {
         setMap(map);
-        map.fitBounds(bounds);
+
+        myMarkers.addTo(map);
+        setMyMarkers(myMarkers)
+        map.fitBounds(mapBounds);
     }
 
     return (
-        <MapContainer whenCreated={setMapReference} crs={CRS.Simple}>
-            <ImageOverlay
-                url="https://i.pinimg.com/originals/bc/59/91/bc5991a551a011777e72aefa7e914ba8.jpg"
-                bounds={bounds}
-            />
-        </MapContainer>
+        <div id="mapid">
+            <MapContainer
+                center={[0, 0]}
+                whenCreated={setMapReference}
+                minZoom={-5}
+                crs={CRS.Simple}>
+                <ImageOverlay
+                    url="https://i.pinimg.com/originals/bc/59/91/bc5991a551a011777e72aefa7e914ba8.jpg"
+                    bounds={mapBounds}
+                    zIndex={-1}
+                />
+                <Marker position={[430.505, 1150.09]}>
+                    <Popup>
+                        <LiteYouTubeEmbed
+                            id="_o7gHiAIYkA"
+                            title="LOTRO | House of Healing Music And Ambience | Minas Tirith"
+                        />
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     );
 };

db691a748019e1ca992115da4a1ad2663a31114a
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2022-07-15 21:04:12 +0100

    Add new images

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,5 +1,6 @@
 // Leaflet Map
-export const MAP_URL = 'https://sounds-of-middle-earth.s3.eu-west-1.amazonaws.com/MiddleEarthUpscaled.webp';
+// export const MAP_URL = 'https://sounds-of-middle-earth.s3.eu-west-1.amazonaws.com/MiddleEarthUpscaled.webp';
+export const MAP_URL = '../assets/img/MiddleEarthUpscaled.jpg';
 export const MAP_Y = 5656;
 export const MAP_X = 8000;
 

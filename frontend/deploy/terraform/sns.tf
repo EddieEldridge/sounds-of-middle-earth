271fc9813580952eef84f4b3f2c324d42f152c3e
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2023-03-10 15:22:29 +0000

    Allow lambda access

--- frontend/deploy/terraform/sns.tf
+++ frontend/deploy/terraform/sns.tf
@@ -7,3 +7,41 @@ resource "aws_sns_topic_subscription" "feedback_topic_subscription" {
   protocol  = "sms"
   endpoint  = var.my_phone_number
 }
+
+resource "aws_sns_topic_policy" "feedback_topic_policy" {
+  arn = aws_sns_topic.feedback_topic.arn
+
+  policy = data.aws_iam_policy_document.feedback_topic_policy_document.json
+}
+
+data "aws_iam_policy_document" "feedback_topic_policy_document" {
+  policy_id = "some_feedback_policy_id"
+
+  statement {
+    actions = [
+      "SNS:Publish"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        "arn:aws:iam::775399702380:root"
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.feedback_topic.arn,
+    ]
+
+    sid = "some_feedback_policy_sid"
+  }
+}

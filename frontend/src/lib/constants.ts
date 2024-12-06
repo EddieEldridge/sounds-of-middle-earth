80c296c2fd75e99c139bd3dda0a5809150998c0a
Eddie Eldridge <steadyeddie101@hotmail.co.uk>
2024-12-06 13:52:35 +0000

    Try fix path

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,7 +1,8 @@
 // Leaflet Map
 // export const MAP_URL = 'https://sounds-of-middle-earth.s3.eu-west-1.amazonaws.com/MiddleEarthUpscaled.webp';
 export const OG_MAP_URL = 'https://i.imgur.com/22R6mSA.jpg';
-export const NEW_MAP_URL = '../assets/img/MiddleEarthBlanco.webp';
+import mapImageUrl from '../assets/img/MiddleEarthBlanco.webp';
+export const NEW_MAP_URL = mapImageUrl;
 export const MAP_Y = 5656;
 export const MAP_X = 8000;
 export const BASE_URL = 'https://fdn17wuw0h.execute-api.eu-west-1.amazonaws.com/dev/api/v1';
